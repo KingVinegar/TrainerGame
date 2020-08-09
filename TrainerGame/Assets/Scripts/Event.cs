627c18e9ff4e5b62cbe0c08a2986185046df5e87
Brian Nielsen <47045697+brnielsen@users.noreply.github.com>
2020-08-09 10:41:12 -0400

    Event script

---
+++ TrainerGame/Assets/Scripts/Event.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Event : MonoBehaviour
+{
+    private PlayerStats playerStats;
+
+    private float timeRan;
+    public float eventMaxTime;
+    public float eventMinTime;
+
+    void Awake()
+    {
+        playerStats = FindObjectOfType<PlayerStats>();
+    
+    }
+
+    public void RunEvent()
+    {
+        float statsFactor = (((1.0f / playerStats.power) + (1.0f / playerStats.technique)) / 2.0f) + 1.0f;
+        float fatigueFactor = 1.0f + (playerStats.fatigue / 100.0f);
+        timeRan = eventMinTime * statsFactor * fatigueFactor;
+
+        Debug.Log("Stats Factor equals " + statsFactor);
+        Debug.Log("Fatigue Factor equals " + fatigueFactor);
+        Debug.Log("Time ran = " + timeRan);
+
+    }
+}
