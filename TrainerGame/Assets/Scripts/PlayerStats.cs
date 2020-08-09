568a5f19c867c7896497e612924cc49d55d7ca3e
Brian Nielsen <47045697+brnielsen@users.noreply.github.com>
2020-08-08 21:33:28 -0400

    Added stats and training scripts

---
+++ TrainerGame/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    [Header ("Player Stats")]
+    public int power = 1;
+    public int powerLevel = 0;
+    public int technique = 1;
+    public int techniqueLevel = 0;
+    public int endurance = 1;
+    public int enduranceLevel = 0;
+    public int conditioning = 1;
+    public int conditioningLevel = 0;
+
+    [Header("Player Conditions")]
+    public int fatigue = 0;
+
+    public int mood = 1;
+
+    public void AddToStat(int stat, int amount)
+    {
+        stat += amount;
+    }
+
+
+}
---
+++ TrainerGame/Assets/Scripts/Training.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Training : MonoBehaviour
+{
+    public string trainingModality = "Training Modality";
+
+    public int changePowerBy = 0;
+    public int changeTechniqueBy = 0;
+    public int changeEnduranceBy = 0;
+    public int changeConditioningBy = 0;
+
+    private PlayerStats playerStats;
+
+    void Awake()
+    {
+        playerStats = FindObjectOfType<PlayerStats>();
+    }
+
+    public void Train()
+    {
+        playerStats.powerLevel += changePowerBy;
+        playerStats.techniqueLevel += changeTechniqueBy;
+        playerStats.enduranceLevel += changeEnduranceBy;
+        playerStats.conditioningLevel += changeConditioningBy;
+    }
+
+}
