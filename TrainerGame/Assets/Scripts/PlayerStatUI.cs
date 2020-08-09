2bb4219ec6bbe0c674347d118615ed5e1f4235b4
Brian Nielsen <47045697+brnielsen@users.noreply.github.com>
2020-08-09 01:04:50 -0400

    Fatigue emoji display wiring.

--- TrainerGame/Assets/Scripts/PlayerStatUI.cs
+++ TrainerGame/Assets/Scripts/PlayerStatUI.cs
@@ -17,7 +17,14 @@ public class PlayerStatUI : MonoBehaviour
     public Image techniqueGauge;
     public Image enduranceGauge;
     public Image conditionGauge;
-        
+
+
+    [Header("Fatigue")]
+    public TextMeshProUGUI fatigueStat;
+    public Sprite[] emojis;
+
+    private Image currentEmoji;
+
     private PlayerStats playerStats;
 
     void Awake()
@@ -36,8 +43,17 @@ public class PlayerStatUI : MonoBehaviour
         powerGauge.fillAmount = playerStats.powerLevel / 100;
         techniqueGauge.fillAmount = playerStats.techniqueLevel / 100;
         enduranceGauge.fillAmount = playerStats.enduranceLevel / 100;
-        conditionGauge.fillAmount = playerStats.conditioningLevel / 100;
+        conditionGauge.fillAmount = playerStats.conditioningLevel / 100;      
 
+    }
+
+    public void CheckFatigueLeve()
+    {
+        int fatigue = playerStats.fatigue;
+
+        fatigueStat.text = fatigue.ToString();
+
+        currentEmoji.sprite = emojis[fatigue - 1];
     }
 }
 
