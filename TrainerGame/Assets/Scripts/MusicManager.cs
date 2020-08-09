75e20ca95ee5f907f221a2147bf4d66860013c7b
Brian Nielsen <47045697+brnielsen@users.noreply.github.com>
2020-08-09 13:29:58 -0400

    Music manager

---
+++ TrainerGame/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public static MusicManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
