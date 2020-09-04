9d1690d14192a46562fd604653bb159f04cb9b2e
Brian Nielsen <47045697+brnielsen@users.noreply.github.com>
2020-09-04 13:23:13 -0400

    Prototyping Menu States (Training and Calendar)

---
+++ TrainerGame/Assets/Scripts/StateMachine/TrainingScreenMessageHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainingScreenMessageHandler : MonoBehaviour
+{
+    private bool calendarOpen;
+
+    void OpenCalendar()
+    {
+        calendarOpen = true;
+    }
+
+    void CloseCalendar()
+    {
+        calendarOpen = false;
+    }
+}
---
+++ TrainerGame/Assets/Scripts/StateMachine/TrainingScreenParameterProvider.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainingScreenParameterProvider : MonoBehaviour
+{
+    private static readonly int calendarOpenId = Animator.StringToHash("CalendarOpen");
+
+    [SerializeField] private Animator animator;
+
+    private bool calendarOpen;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if(calendarOpen == false)
+            {
+                calendarOpen = true;
+            }else if(calendarOpen == true)
+            {
+                calendarOpen = false;
+            }
+        }
+
+        animator.SetBool(calendarOpenId, calendarOpen);
+    }
+
+    void OpenCalendar()
+    {
+        calendarOpen = true;
+    }
+
+    void CloseCalendar()
+    {
+        calendarOpen = false;
+    }
+}
