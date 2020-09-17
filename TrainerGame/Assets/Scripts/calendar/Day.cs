0215f317cb9d4c9c68eb95f9ec68b6e65c9aaa1d
brnielsen <47045697+brnielsen@users.noreply.github.com>
2020-09-17 19:36:14 -0400

    Day Code

---
+++ TrainerGame/Assets/Scripts/calendar/Day.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Day : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI dayText;
+
+    [SerializeField]
+    private int dayOfTheMonth;
+
+    [SerializeField]
+    private Image eventImage;
+
+    private void Start()
+    {
+        dayText.text = dayOfTheMonth.ToString();
+
+
+    }
+
+}
